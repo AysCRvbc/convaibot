5f9e2d6d56e08ae4f59ff95865d4df5e4460c87e
Cludi <cyrsiaesixd@gmail.com>
2024-01-28 22:23:04 +0400

    gpt test

--- gpt.py
+++ gpt.py
@@ -3,7 +3,8 @@ import g4f
 
 def get_resp(system_context, text):
     response = g4f.ChatCompletion.create(
-        model=g4f.models.gpt_35_turbo_16k_0613,
+        model=g4f.models.default,
+        provider=g4f.Provider.GeekGpt,
         messages=[{"role": "user", "content": system_context}, {"role": "user", "content": text}],
     )
     return response
