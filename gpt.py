1f02f1fe0d6fa56102f1557f36ffae05d16afeb5
Cludi <cyrsiaesixd@gmail.com>
2024-02-03 15:48:06 +0400

    fix test

--- gpt.py
+++ gpt.py
@@ -3,7 +3,7 @@ import g4f
 
 def get_resp(system_context, text):
     response = g4f.ChatCompletion.create(
-        model=g4f.models.default,
+        model="gpt-3.5-turbo",
         messages=[{"role": "user", "content": system_context}, {"role": "user", "content": text}],
     )
     return response
